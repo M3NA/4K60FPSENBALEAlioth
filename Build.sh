72ff6646b030bebf6322a6197ab0d3605689b427
Mena <menamaged150@gmail.com>
2022-06-03 17:09:22 +0200

    ..

--- Build.sh
+++ Build.sh
@@ -4,6 +4,6 @@ source module.prop
 
 echo -e "\033[1;31m\Building $id.zip"
 
-zip $id.zip common/ config.sh module.prop system/ META-INF/
+zip -r $id.zip customize.sh module.prop system/ META-INF/
 echo -e "\033[1;31mdone"
 exit 
